a7599d12dc09dba82f700bfef20ddc71663287f8
Audrey <audreywrong@gmail.com>
2021-03-15 21:33:46 -0500

    test backend setup

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\TileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +14,43 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::any('/test', function (Request $request) {
+    $data = json_decode('{
+        "discussion": 1156651487.379289,
+        "song": "appearance",
+        "congress": -711955873,
+        "difficulty": {
+          "grade": false,
+          "like": "shadow",
+          "price": -860806398,
+          "drawn": 1274897875.5733166,
+          "opposite": {
+            "have": true,
+            "so": true,
+            "wise": true,
+            "son": "wash",
+            "getting": "cat",
+            "we": false
+          },
+          "fought": "couple"
+        },
+        "too": -1919765016.0258815,
+        "year": 1233461737.3856227
+       }');
+    return response()->json([
+        'data' => $data,
+    ]);
+});
+
+Route::any('/push', [TestController::class, 'store']);
+Route::get('/fetch/tiles', [TileController::class, 'index']);
+Route::get('/fetch/tile/{id}', [TileController::class, 'show']);
+Route::post('/store/tile', [TileController::class, 'store']);
+Route::post('/update/tile/{id}', [TileController::class, 'update']);
+Route::post('/destroy/tile/{id}', [TileController::class, 'destroy']);
