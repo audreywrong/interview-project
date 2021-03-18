65a2a7c27ca457fc47ff43973c6d568f553598a3
Audrey <audreywrong@gmail.com>
2021-03-18 17:01:47 -0500

    styles and cleanup
    cleaned up profile component
    applied some basic styling

--- resources/js/components/Profile.js
+++ resources/js/components/Profile.js
@@ -1,64 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import "../../css/app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 const Profile = () => {
+    const [username, setUsername] = useState(" ");
+
+    // const useEffect = () => {
+    //     const url = "http://127.0.0.1:8000/api/user";
+
+    //     axios.get(url).then(function (response) {
+    //         // handle success
+    //         let data = response.data;
+    //         console.log(data);
+    //         setUsername(data);
+    //     });
+    // };
+
     return (
         <>
-            <div class="container">
-                <div class="row">
-                    <div class="col-3 p-5">
+            <div className="container">
+                <div className="profile">
+                    <div className="profile-pic-container">
                         <img
                             src="https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?tp=1&amp;_nc_ht=scontent-dfw5-2.cdninstagram.com&amp;_nc_ohc=9tzq_P07DRYAX_RrUup&amp;oh=0998f21181c37b9fba0572f163ec66c6&amp;oe=60736467"
-                            class="rounded-circle"
+                            className="profile-pic"
                         />
                     </div>
-                    <div class="col-9 pt-5">
+                    <div className="profile-information">
                         <div>
-                            <h1>USERNAME PLACEHOLDER</h1>
+                            <h1>Welcome {username}!</h1>
                         </div>
-                        <div class="d-flex">
-                            <div class="pr-5">
-                                <strong>153</strong> posts
+                        <div className="d-flex">
+                            <div className="pr-5">
+                                <strong>6</strong> posts
                             </div>
-                            <div class="pr-5">
-                                <strong>23k</strong> followers
+                            <div className="pr-5">
+                                <strong>2,465</strong> followers
                             </div>
-                            <div class="pr-5">
+                            <div className="pr-5">
                                 <strong>212</strong> following
                             </div>
                         </div>
-                        <div class="pt-4 font-weight-bold">
-                            freeCodeCamp.org
+                        <div className="pt-4 font-weight-bold">
+                            Music Taste Summary
                         </div>
                         <div>
-                            We're a global community of millions of people
-                            learning to code together. We're an open source,
-                            donor-supported, 501(c)(3) nonprofit.
+                            I love a little bit of everything! From Prince to
+                            Biggie Smalls to Nightwish and Metallica -- I rarely
+                            meet a band I don't like! Always looking for new
+                            music to check out.
                         </div>
                         <div>
-                            <a href="#">www.freecodecamp.org</a>
+                            <a href="#">could put a link to something here</a>
                         </div>
                     </div>
                 </div>
 
-                <div class="row pt-5">
-                    <div class="col-4">
+                <div className="row">
+                    <div className="col-4">
                         <img
                             src="https://scontent-dfw5-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.129.1034.1034a/s640x640/159113925_224405209391401_5023734514820725236_n.jpg?tp=1&_nc_ht=scontent-dfw5-1.cdninstagram.com&_nc_cat=101&_nc_ohc=ATuBXzXbXIwAX-nKTcL&oh=35d22c28e3855cc9f92e71f70c0feaf7&oe=60758DD9"
-                            class="w-100"
+                            className="w-100"
                         />
                     </div>
-                    <div class="col-4">
+                    <div className="col-4">
                         <img
                             src="https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/157712100_495573674790980_9025787082838431165_n.jpg?tp=1&_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_cat=108&_nc_ohc=USqWyjiP-acAX9lxBk2&oh=1346a875205dd083773b421b62084b1c&oe=6073C09F"
-                            class="w-100"
+                            className="w-100"
                         />
                     </div>
-                    <div class="col-4">
+                    <div className="col-4">
                         <img
                             src="https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.128.1034.1034a/s640x640/155930641_449057102970296_7520574586017729636_n.jpg?tp=1&_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_cat=108&_nc_ohc=w5BVTYRm-ZcAX-3aNwY&oh=93eac034dc94672bbef84b0acfc93d87&oe=60767F0B"
-                            class="w-100"
+                            className="w-100"
                         />
                     </div>
                 </div>
