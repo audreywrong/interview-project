207ecdb365141ba21c07826bd1f307a24b897e41
Audrey <audreywrong@gmail.com>
2021-03-18 12:28:41 -0500

    changes to views
    changes to views to create a better user dashboard experience

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.app')
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    @auth
+    <script>
+        var GUSER = {{ Auth::user()->id }};
+    </script>
+    @endauth
+
+
+
+</head>
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+            <div class="antialiased">
+                <div id="band-form"></div>
             </div>
         </div>
     </div>
+    {{-- <script defer src="{{ asset('/js/app.js') }}"></script> --}}
+    <style src="{{ asset('/css/app.css') }}"></style>
 </div>
+
 @endsection
